cc3c162f2c1a2728394add8240bcac3f40513370
Baronus de Somnia <marimorovichk@gmail.com>
2024-03-05 21:06:34 +0200

    first commit

---
+++ RiasEngineEditor/RiasEngineCore/RiasEngineCore/OGL3D/include/OGL3D/Game/OGame.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <memory>
+class OWindow;
+class OGame
+{
+public:
+	OGame();
+	~OGame();
+	void run();
+	void quit();
+protected:
+	bool m_isRunnig = true;
+	std::unique_ptr<OWindow> m_display ;
+};
+
